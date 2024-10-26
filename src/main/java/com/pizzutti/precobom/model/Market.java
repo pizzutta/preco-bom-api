41c21e6ff306ceaae1af44fb7ae8532ccb10eb6f
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-10-25 21:07:11 -0300

    docs: documenting market object

--- src/main/java/com/pizzutti/precobom/model/Market.java
+++ src/main/java/com/pizzutti/precobom/model/Market.java
@@ -1,24 +1,32 @@
 package com.pizzutti.precobom.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity(name = "tb_market")
+@Schema(description = "A market object contains information about a market registered in the app")
 public class Market {
 
     @Id
     @GeneratedValue
+    @Schema(description = "The market ID", example = "1")
     private Long id;
     @Column
+    @Schema(description = "The market's name", example = "QuickCart")
     private String name;
     @Column
+    @Schema(description = "The market's address", example = "123 Oak Ave, Everdale, ZIP 54321")
     private String address;
     @Column
+    @Schema(description = "An URL or a path to an image provided by the market to represent it",
+            example = "https://mysite.com/images/my-logo.jpg")
     private String image;
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "market")
     @JsonBackReference
+    @Schema(description = "A list of product objects registered by the market")
     private List<Product> products;
 
     public Long getId() {
