8298210c4afdb322fe02ff0b0d64b1c0235fb4ee
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-10-30 14:51:21 -0300

    docs: documenting item list object

--- src/main/java/com/pizzutti/precobom/model/ItemList.java
+++ src/main/java/com/pizzutti/precobom/model/ItemList.java
@@ -1,18 +1,25 @@
 package com.pizzutti.precobom.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 @Entity(name = "tb_item_list")
+@Schema(description = "An item list object contains information about an item of a grocery list in the app")
 public class ItemList {
+
     @Id
     @GeneratedValue
+    @Schema(description = "The item list ID", example = "1")
     private Long id;
     @ManyToOne
     @JoinColumn(name = "product_id")
+    @Schema(description = "The product that is contained in the grocery list")
     private Product product;
     @Column
+    @Schema(description = "The quantity defined by the user for that product", example = "5")
     private Integer quantity;
     @Column
+    @Schema(description = "If true, the item has already been checked on the grocery list")
     private Boolean checked;
 
     public Long getId() {
