914bd408574b2c8243f7142a058bf419d862dd71
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-10-30 14:31:40 -0300

    docs: documenting user object

--- src/main/java/com/pizzutti/precobom/model/User.java
+++ src/main/java/com/pizzutti/precobom/model/User.java
@@ -1,5 +1,6 @@
 package com.pizzutti.precobom.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,18 +10,22 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity(name = "tb_user")
+@Schema(description = "An user object contains information about an user registered in the app")
 public class User implements UserDetails {
 
     @Id
     @GeneratedValue
+    @Schema(description = "The user ID", example = "1")
     private Long id;
     @Column
+    @Schema(description = "The user's email", example = "my@email.com")
     private String email;
     @Column
+    @Schema(hidden = true)
     private String password;
     @Enumerated(EnumType.STRING)
+    @Schema(description = "The user's role in the app", example = "ADMIN")
     private UserRole role;
-
 
     public Long getId() {
         return id;
@@ -56,32 +61,38 @@ public class User implements UserDetails {
     }
 
     @Override
+    @Schema(hidden = true)
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return (this.role == UserRole.ADMIN) ? List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"))
                 : List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
+    @Schema(hidden = true)
     public String getUsername() {
         return email;
     }
 
     @Override
+    @Schema(hidden = true)
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @Schema(hidden = true)
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @Schema(hidden = true)
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @Schema(hidden = true)
     public boolean isEnabled() {
         return true;
     }
