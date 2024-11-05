eba97b941b52a6ec711bcb44c3f04fa9af306f8c
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-11-05 10:00:06 -0300

    docs: documenting IdDTO

--- src/main/java/com/pizzutti/precobom/dto/IdDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/IdDTO.java
@@ -1,9 +1,12 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "A data transfer object that contains information an ID number")
 public record IdDTO(
         @NotNull
+        @Schema(description = "The ID", example = "1")
         Long id
 ) {
 }
