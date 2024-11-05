5174d470cf06484948f61ff00d911919eb30166d
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-11-05 09:34:53 -0300

    docs: documenting grocery list dtos

--- src/main/java/com/pizzutti/precobom/dto/GroceryListRegisterDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/GroceryListRegisterDTO.java
@@ -1,12 +1,16 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "A data transfer object that contains information to register a grocery list")
 public record GroceryListRegisterDTO(
         @NotBlank
+        @Schema(description = "The grocery list's name", example = "Weekly Essentials")
         String name,
         @NotNull
+        @Schema(description = "The user ID", example = "1")
         Long userId
 ) {
 }
--- src/main/java/com/pizzutti/precobom/dto/GroceryListUpdateDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/GroceryListUpdateDTO.java
@@ -1,12 +1,16 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "A data transfer object that contains information to update a grocery list")
 public record GroceryListUpdateDTO(
         @NotNull
+        @Schema(description = "The grocery list ID", example = "1")
         Long id,
         @NotBlank
+        @Schema(description = "The grocery list's name", example = "Weekly Essentials")
         String name
 ) {
 }
