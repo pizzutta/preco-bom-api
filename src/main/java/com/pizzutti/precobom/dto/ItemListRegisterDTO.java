d93a8f94f8f64cbb7cd9717a77a4a8f5552d5f72
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-11-05 09:38:45 -0300

    docs: documenting item list dtos

--- src/main/java/com/pizzutti/precobom/dto/ItemListRegisterDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/ItemListRegisterDTO.java
@@ -1,17 +1,23 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+@Schema(description = "A data transfer object that contains information to register an item list")
 public record ItemListRegisterDTO(
         @NotNull
+        @Schema(description = "The product ID", example = "1")
         Long productId,
         @NotNull
         @Positive
+        @Schema(description = "The quantity defined for that product", example = "5")
         Integer quantity,
         @NotNull
+        @Schema(description = "If true, the item has already been checked on the grocery list")
         Boolean checked,
         @NotNull
+        @Schema(description = "The grocery list ID", example = "1")
         Long groceryListId
 ) {
 }
--- src/main/java/com/pizzutti/precobom/dto/ItemListUpdateDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/ItemListUpdateDTO.java
@@ -1,15 +1,19 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record ItemListUpdateDTO(
         @NotNull
+        @Schema(description = "The item list ID", example = "1")
         Long id,
         @NotNull
         @Positive
+        @Schema(description = "The quantity defined for that product", example = "5")
         Integer quantity,
         @NotNull
+        @Schema(description = "If true, the item has already been checked on the grocery list")
         Boolean checked
 ) {
 }
