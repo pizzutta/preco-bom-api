84d6c562ea2fdda48ed6dfbbf63265cd71c2ad40
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-11-05 09:16:41 -0300

    docs: documenting market dtos

--- src/main/java/com/pizzutti/precobom/dto/MarketRegisterDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/MarketRegisterDTO.java
@@ -1,13 +1,19 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "A data transfer object that contains information to register a market")
 public record MarketRegisterDTO(
         @NotBlank
+        @Schema(description = "The market's name", example = "QuickCart")
         String name,
         @NotBlank
+        @Schema(description = "The market's address", example = "123 Oak Ave, Everdale, ZIP 54321")
         String address,
         @NotBlank
+        @Schema(description = "An URL or a path to an image provided by the market to represent it",
+                example = "https://mysite.com/images/my-logo.jpg")
         String image
 ) {
 }
--- src/main/java/com/pizzutti/precobom/dto/MarketUpdateDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/MarketUpdateDTO.java
@@ -1,16 +1,23 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "A data transfer object that contains information to update a market")
 public record MarketUpdateDTO(
         @NotNull
+        @Schema(description = "The market ID", example = "1")
         Long id,
         @NotBlank
+        @Schema(description = "The market's name", example = "QuickCart")
         String name,
         @NotBlank
+        @Schema(description = "The market's address", example = "123 Oak Ave, Everdale, ZIP 54321")
         String address,
         @NotBlank
+        @Schema(description = "An URL or a path to an image provided by the market to represent it",
+                example = "https://mysite.com/images/my-logo.jpg")
         String image
 ) {
 }
