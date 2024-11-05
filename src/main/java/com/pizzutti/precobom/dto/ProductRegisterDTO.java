0669b58531719d0f86cac0c997026dfe6b82c3ad
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-11-05 09:20:37 -0300

    docs: documenting product dtos

--- src/main/java/com/pizzutti/precobom/dto/ProductRegisterDTO.java
+++ src/main/java/com/pizzutti/precobom/dto/ProductRegisterDTO.java
@@ -1,18 +1,26 @@
 package com.pizzutti.precobom.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "A data transfer object that contains information to register a product")
 public record ProductRegisterDTO(
         @NotBlank
+        @Schema(description = "The product's name", example = "Grape Tomatoes")
         String name,
         @NotNull
+        @Schema(description = "The product's price determined by the market", example = "2.25")
         Double price,
         @NotBlank
+        @Schema(description = "The unit used to measure the product", example = "lbs")
         String measuringUnit,
         @NotBlank
+        @Schema(description = "An URL or a path to an image provided by the market to represent the product",
+                example = "https://mysite.com/images/my-product.jpg")
         String image,
         @NotNull
+        @Schema(description = "The market ID", example = "1")
         Long marketId
 ) {
 }
