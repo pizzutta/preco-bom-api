f467ec0673b629336d0df52353620e601d6aec34
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-11-05 23:34:23 -0300

    docs: documenting authentication endpoints

--- src/main/java/com/pizzutti/precobom/controller/AuthenticationController.java
+++ src/main/java/com/pizzutti/precobom/controller/AuthenticationController.java
@@ -7,6 +7,12 @@ import com.pizzutti.precobom.dto.RegisterDTO;
 import com.pizzutti.precobom.model.User;
 import com.pizzutti.precobom.repository.UserRepository;
 import com.pizzutti.precobom.security.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +25,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("auth")
+@Tag(name = "Authentication")
 public class AuthenticationController {
+
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -30,8 +40,16 @@ public class AuthenticationController {
     private TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
-        UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+    @Operation(description = "Logs in a user in the app")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User logged",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = LoginResponseDTO.class))),
+            @ApiResponse(responseCode = "401", description = "Login failed", content = @Content)
+    })
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO data) {
+        UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(data.email(),
+                data.password());
         Authentication auth = this.authenticationManager.authenticate(usernamePassword);
 
         User user = (User) auth.getPrincipal();
@@ -41,7 +59,13 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+    @Operation(description = "Registers a new user in the app")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Request content is invalid", content = @Content)
+    })
+    public ResponseEntity<User> register(@RequestBody @Valid RegisterDTO data) {
         if (this.repository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
@@ -52,6 +76,7 @@ public class AuthenticationController {
 
         this.repository.save(user);
 
-        return ResponseEntity.ok().build();
+        //TODO: implement UserController?
+        return ResponseEntity.created(URI.create("")).body(user);
     }
 }
