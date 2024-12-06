41a9c4d218a0514cc3c8740be93b6096e5f284f8
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-12-06 15:33:41 -0300

    feat: protecting put and delete endpoints for market and product

--- src/main/java/com/pizzutti/precobom/security/SecurityConfigurations.java
+++ src/main/java/com/pizzutti/precobom/security/SecurityConfigurations.java
@@ -3,17 +3,18 @@ package com.pizzutti.precobom.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +26,7 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
                                 "/api-docs",
@@ -33,10 +34,14 @@ public class SecurityConfigurations {
                                 "/swagger-ui/index.html",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/market").hasRole("ADMIN")
+                        .requestMatchers(POST, "/auth/login").permitAll()
+                        .requestMatchers(POST, "/auth/register").permitAll()
+                        .requestMatchers(POST, "/product").hasRole("ADMIN")
+                        .requestMatchers(PUT, "/product").hasRole("ADMIN")
+                        .requestMatchers(DELETE, "/product").hasRole("ADMIN")
+                        .requestMatchers(POST, "/market").hasRole("ADMIN")
+                        .requestMatchers(PUT, "/market").hasRole("ADMIN")
+                        .requestMatchers(DELETE, "/market").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
